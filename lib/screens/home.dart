592575683f7c7070e8c7d03f564f9e975d5bc669
RUTAHIGWA EMMANUEL NOEL <77039776+EmmanuelNoel01@users.noreply.github.com>
2024-11-18 23:12:18 +0300

    changed for a webview to a url launcher

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,91 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
-import 'dart:io';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:open_filex/open_filex.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  late WebViewController webViewController;
-  bool isLoading = true;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _initializeDownloader();
-    webViewController = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(NavigationDelegate(
-        onPageFinished: (url) {
-          setState(() {
-            isLoading = false;
-          });
-        },
-        onNavigationRequest: (request) {
-          if (_isDownloadableFile(request.url)) {
-            _downloadFile(request.url);
-            return NavigationDecision.prevent; 
-          }
-          return NavigationDecision.navigate; 
-        },
-      ))
-      ..loadRequest(Uri.parse("https://agency.sanlam4u.co.ug"))
-      ..clearCache();
-  }
-
-  Future<void> _initializeDownloader() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await FlutterDownloader.initialize(debug: true);
-  }
-
-  bool _isDownloadableFile(String url) {
-    return url.endsWith('.pdf') || url.endsWith('.zip') || url.endsWith('.docx') || url.endsWith('.xlsx');
-  }
-
-  Future<void> _downloadFile(String url) async {
-    if (Platform.isAndroid) {
-      final status = await Permission.storage.request();
-      if (status.isDenied) {
-        throw 'Storage permission is needed to download files.';
-      }
-    }
-
-    final taskId = await FlutterDownloader.enqueue(
-      url: url,
-      savedDir: '/storage/emulated/0/Download',
-      showNotification: true,
-      openFileFromNotification: true,
-    );
-
-    // Open the file after downloading
-    FlutterDownloader.registerCallback((id, status, progress) async {
-      if (status == DownloadTaskStatus.complete) {
-        // The file is downloaded, now we can open it
-        final filePath = '/storage/emulated/0/Download/${url.split('/').last}';
-        OpenFilex.open(filePath);
-      }
+    Future.delayed(Duration(seconds: 5), () async {
+      await launchUrl(Uri.https("agency.sanlam4u.co.ug"));
+      setState(() {
+        _isLoading = false;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            WebViewWidget(controller: webViewController),
-            if (isLoading)
-              Center(
-                child: CircularProgressIndicator(),
-              ),
-          ],
+      body: SizedBox.expand(
+        child: Center(
+          child: _isLoading
+              ? CircularProgressIndicator() 
+              : Container(), 
         ),
       ),
     );
